65b9bad2deb9e389c847c9b4d451094c5f812dba
Qgnah <1139948347@qq.com>
2022-05-25 10:31:23 +0800

    第一个git测试

---
+++ src/test/java/com/qgnah/gitstudy/GitstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qgnah.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
